e5b7e64c68920234e6b95fae032e30e4d95cd842
MANASA KALAGA <manasakalaga@koala.local>
2023-02-05 10:45:45 +0530

    content changes

--- app/blogs/page.tsx
+++ app/blogs/page.tsx
@@ -1,13 +1,11 @@
-export default function ArtsPage() {
+export default function BlogsPage() {
     return (
       <section>
         <h1 className="font-bold text-3xl font-serif">Blogs</h1>
         <br></br>
-        
-        
+        <p>Coming soon! 📍</p>
         <div className="prose prose-neutral dark:prose-invert text-neutral-800 dark:text-neutral-200">
         <hr />
-        <p>👀 Coming soon!</p>
         </div>
       </section>
     );
