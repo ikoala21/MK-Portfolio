c7c95616b4a793f26966526600085cfeb9021cf3
MK <sreemanasa.2000@gmail.com>
2023-09-17 00:55:02 +0530

    removed first two images

--- app/arts/page.tsx
+++ app/arts/page.tsx
@@ -10,26 +10,6 @@ export default function ArtsPage() {
         <hr />
 
         <div className="flex flex-wrap">
-          <div className="mb-4 mr-4 w-2/3 sm:w-1/3">
-            <img
-              src="/art01.jpg"
-              alt="Art Image1"
-              className="w-full h-auto rounded-lg transform hover:scale-105 transition-transform duration-200 object-cover"
-              style={{ width: '100%', height: '100%' }}
-            />
-          </div>
-
-          <div className="mb-4 w-2/3 sm:w-1/3">
-            <img
-              src="/art02.jpg"
-              alt="Art Image2"
-              className="w-full h-auto rounded-lg transform hover:scale-105 transition-transform duration-200 object-cover"
-              style={{ width: '100%', height: '100%' }}
-            />
-          </div>
-        </div>
-
-        <div className="flex flex-wrap">
           <div className="mb-4 mr-4 w-1/3 sm:w-1/3">
             <img
               src="/art03.jpg"
